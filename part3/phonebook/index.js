5619b827fe859cf59a9a1d507fcecc1866a01814
Kerkko Kuokkanen <kerkko.kuokkanen@gmail.com>
2023-04-10 16:48:34 +0300

    mongo

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,15 +1,16 @@
 
 const http = require('http')
 
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./modules/person')
+const { resolve } = require('path')
 
 app.use(express.json())
 app.use(cors())
-  
-//app.use(morgan('tiny'))
 
 morgan.token('body', (request) => JSON.stringify(request.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
@@ -43,20 +44,49 @@ let persons = [
     response.send('<h1>Hello World!</h1>')
   })
 
-  app.get('/info', (request, response) => {
-    response.send(`<div>Phonebook has info for ${persons.length} people <br />
-        ${new Date()}</div>`)
+  app.get('/info', (request, response, next) => {
+    Person.countDocuments({})
+    .then(count => {
+      response.send(`<div>Phonebook has info for ${count} people <br />
+      ${new Date()}</div>`)
+    })
+    .catch(error => next(error))
   })
 
-  app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
+  app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+      .then(result => {
+        response.status(204).end()
+      })
+      .catch(error => next(error))
   })
   
   app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(person => {
+      response.json(person)
+    })
+  })
+
+  app.put('/api/persons/:id', (request, response, next) => {
+    const entry = request.body
+    const id = request.params.id
+
+    if (!entry.number) {
+      return response.status(400).json({
+        error: 'number missing'
+      })
+    }
+
+    const person = {
+      name: entry.name,
+      number: entry.number,
+    }
+
+    Person.findByIdAndUpdate(id, person, {new: true}) //found this {new:true} thing from chatGPT. Ensures that the updated person is a new person and not the old one
+      .then(updatedPerson => {
+        response.json(updatedPerson)
+      })
+      .catch(error => next(error))
   })
 
   app.post('/api/persons', (request, response) => {
@@ -80,25 +110,42 @@ let persons = [
         })
       }
 
-    const person = {
-        name: entry.name,
-        number: entry.number,
-        id: Math.random() * Number.MAX_SAFE_INTEGER
-    }
+    const person = new Person({
+      name: entry.name,
+      number: entry.number,
+    })
     
-    persons = persons.concat(person)
-    response.json(person)
+    person.save().then(addedPerson => {
+      response.json(addedPerson)
+    })
   })
 
-  app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    if (person)
+  app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id).then(person => {
+      if (person)
         response.json(person)
-    else
+      else
         response.status(404).end()
+    })
+    .catch(error => next(error))
   })
 
+  const unknownEndpoint = (request, response) => {
+    response.status(404).send({error: 'unknown endpoint'})
+  }
+
+  app.use(unknownEndpoint)
+
+  const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+    console.log(error.name)
+    if (error.name === 'CastError')
+      return (response.status(400).send({error: 'malformatted id'}))
   
-  const PORT = process.env.PORT || 3001
+    next(error)
+  }
+  
+  app.use(errorHandler)
+  
+  const PORT = process.env.PORT
   app.listen(PORT, () => {console.log(`Server running on port ${PORT}`)})
